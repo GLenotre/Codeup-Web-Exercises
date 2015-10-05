07538f4583e0e96acdd688102fc971823800f9ea
user <user@users-MacBook-Pro.local>
2015-10-05 11:07:13 -0500

    Used JS For-Loop with Decrements oof Five

---
+++ public/js/for_loop.js
@@ -0,0 +1,5 @@
+var i = 100
+
+for (var i = 100; i <101, i > 0; i = i - 5) {
+	console.log('' + i);
+}
--- public/js/while_loop.js
+++ public/js/while_loop.js
@@ -1,5 +1,7 @@
 var i = 2
+
 while (i <= 65536) {
 	console.log('while loop iteration #' + i);
 	i= i*2;
 } 
+
