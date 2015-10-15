a32547f90d5bba1059448245a276c1376d20649d
user <user@users-MacBook-Pro.local>
2015-10-15 13:35:30 -0500

    added an alert in js

--- public/js/jquery_exercises.js
+++ public/js/jquery_exercises.js
@@ -1,1 +1,12 @@
-alert('You\'ve uploaded the file!');
+
+$(document).ready(function() {			// DO ONE BIG (document).ready(function()) {} // instead of IEFE, protects from global scope
+//     $('#intro').css('background-color', 'pink');
+//     $('.lookcool').css('color', 'orange');
+//     $('.garamondlook').css('font-family','Garamond');
+//     $('.codeup').css('border','1px solid red');
+//     $('p').css('font-size', '20px');
+     var contents = $('h1').html();
+     alert(contents);
+	$('*').css('border', '1px solid grey');
+
+});
