e474a7733477324e2bea0a0241b6842a844b74f3
user <user@users-MacBook-Pro.local>
2015-10-15 14:32:16 -0500

    jquery hover, click

--- public/js/jquery_exercises.js
+++ public/js/jquery_exercises.js
@@ -1,12 +1,14 @@
 
 $(document).ready(function() {			// DO ONE BIG (document).ready(function()) {} // instead of IEFE, protects from global scope
-//     $('#intro').css('background-color', 'pink');
-//     $('.lookcool').css('color', 'orange');
-//     $('.garamondlook').css('font-family','Garamond');
-//     $('.codeup').css('border','1px solid red');
-//     $('p').css('font-size', '20px');
-     var contents = $('h1').html();
-     alert(contents);
-	$('*').css('border', '1px solid grey');
 
+	$( "h1" ).click(function(e) {
+		$(this).css('background-color','salmon');
+	});
+	$( "p" ).dblclick(function(e){
+		$(this).css('font-size','18px');
+	});
+	$( "li" ).hover(function(e){
+		$(this).css('color','red');
+	});
+	// Set all li text color to red when the mouse is hovering, reset to white when it is not.
 });
