2a8e1004e90375ceff380172ff39f22a57a5992a
user <user@users-MacBook-Pro.local>
2015-10-16 10:24:43 -0500

    comments from lecture

--- public/js/konami_exercises.js
+++ public/js/konami_exercises.js
@@ -1,13 +1,15 @@
 
 $(document).ready(function() {			
 	"use strict";
-	var contra = "38,38,40,40,37,39,37,39,66,65";
+	var contra = "38,38,40,40,37,39,37,39,66,65";  // can turn this into an array proper [38, 38]
 	var answer = []
 
 	 $(document).keyup(function(event) {
 	 	answer.push(event.keyCode);
-	 	if(answer.toString().indexOf(contra) >= 0){
-		$("*").css('background-color','chartreuse').css('font-family', 'Monospace').css('font-size', '40px');
+	 	if(answer.toString().indexOf(contra) >= 0){  // then join the arrays 
+		$("*").css('background-color','chartreuse')
+		.css('font-family', 'Monospace')
+		.css('font-size', '40px');
 		$("p").html("You win!!!");
 		console.log(answer);
 	 	answer = []
