63365e42a0ce35067c912bc6c4963c4d882918fb
user <user@users-MacBook-Pro.local>
2015-10-17 21:08:26 -0500

    Added .hover to change color

--- public/js/jquery_faq.js
+++ public/js/jquery_faq.js
@@ -1,11 +1,12 @@
+"use strict";
 $(document).ready(function() {
-	$('dd').addClass('invisible');	
-$('a').click(function(event) {
-	event.preventDefault();
-	$('dd').toggleClass('invisible');
-	});
-});
+	// $('ul').addClass('invisible');	
+$('*').hover(function(e) {
 
- //add a link that toggles the class invisible on and off for all dd elements
-	
-    //$(this).toggleClass('invisible');
+	e.preventDefault();
+	$('*').css('color','red');
+},
+	function(e) {
+	$('*').css('color','black');
+	 });
+});
